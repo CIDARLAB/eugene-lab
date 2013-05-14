591d5d24bf48503dde804691c4315e585b0b6ed8
Jenhan Tao <jenhantao@gmail.com>
2013-05-13 21:33:59 -0400

    run button can now generate eugene for devices

--- EugeneLab/web/javascript/EugeneLabController.js
+++ EugeneLab/web/javascript/EugeneLabController.js
@@ -44,20 +44,39 @@ $(document).ready(function() {
 
     //Event Handlers
     $('#newDeviceButton').click(function() {
-        if(deviceCount===0){
+        if (deviceCount === 0) {
             $('#spectaclesEditorArea').html("");
         }
         $('#spectaclesEditorArea').append('<ul id="device' + deviceCount + '" class="device droppable sortable"><li class="blank" style="height:80px;width:80px;border:1px solid grey" title="Drag a part here to get started">New Device</li></ul>');
-        $("#device"+deviceCount).sortable({
+        $("#device" + deviceCount).sortable({
             revert: true
         });
-        $("#device"+deviceCount).droppable({
-            drop:function(){
+        $("#device" + deviceCount).droppable({
+            drop: function() {
                 $(this).find("li.blank").remove();
             }
         });
         deviceCount = deviceCount + 1;
-
     });
+
+    $('#runButton').click(function() {
+        $('li.blank').parent().remove();
+        $('li.blank').remove();
+        var command = {};
+        command["command"] = "run";
+        command["devices"] = [];
+        command["parts"] = [];
+        $('#spectaclesEditorArea ul').each(function() {
+            var device = "Device " + $(this).attr("id") + "(";
+            //            command["devices"].push($(this).attr("id"));
+            $(this).find("li").each(function() {
+                device = device + $(this).attr("title").split(' ').join("") + ",";
+            });
+            device = device.substring(0, device.length - 1);
+            device = device + ")";
+            command["devices"].push(device);
+        });
+    });
+
 });
 
